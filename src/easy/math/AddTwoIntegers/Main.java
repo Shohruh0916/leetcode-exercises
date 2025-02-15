9a1e3375e1fd5ba40247ccad7a8d9c2bd3afaeb7
Shohruh <sharipovshohruh@gmail.com>
2025-02-15 16:10:34 +0500

    leetcode

---
+++ src/easy/math/AddTwoIntegers/Main.java
@@ -0,0 +1,9 @@
+package easy.math.AddTwoIntegers;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1=12,num2=5;
+        Solution Solution= new Solution();
+        System.out.println(Solution.addTwoIntegers(num1,num2));
+    }
+}
---
+++ src/easy/math/AddTwoIntegers/Solution.java
@@ -0,0 +1,7 @@
+package easy.math.AddTwoIntegers;
+
+public class Solution {
+    public  int addTwoIntegers(int num1, int num2){
+        return num1+num2;
+    }
+}
