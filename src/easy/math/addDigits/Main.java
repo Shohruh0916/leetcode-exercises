13bacbf5acbf1fce9f938bee9e206b022d8cd46a
Shohruh <sharipovshohruh@gmail.com>
2025-02-20 17:03:41 +0500

    leetcode

---
+++ src/easy/math/addDigits/Main.java
@@ -0,0 +1,9 @@
+package easy.math.addDigits;
+
+public class Main {
+    public static void main(String[] args) {
+        int num=100;
+        Soution soution=new Soution();
+        System.out.println(soution.addDigits(num));
+    }
+}
---
+++ src/easy/math/addDigits/Soution.java
@@ -0,0 +1,10 @@
+package easy.math.addDigits;
+
+public class Soution {
+    public int addDigits(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        return 1 + (num - 1) % 9;
+    }
+}
