37bc943a77304bb50f76486e2ed65fece1432afe
Shohruh <sharipovshohruh@gmail.com>
2025-02-20 13:31:28 +0500

    leetcode

---
+++ src/easy/math/mysqrt/Main.java
@@ -0,0 +1,9 @@
+package easy.math.mysqrt;
+
+public class Main {
+    public static void main(String[] args) {
+        int x=4;
+        Solution solution=new Solution();
+        System.out.println(solution.mySqrt(x));
+    }
+}
---
+++ src/easy/math/mysqrt/Solution.java
@@ -0,0 +1,8 @@
+package easy.math.mysqrt;
+
+public class Solution {
+    public int mySqrt(int x) {
+        double result=Math.sqrt(x);
+        return (int) result;
+    }
+}
