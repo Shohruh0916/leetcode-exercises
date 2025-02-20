4fb9a527f51fb3d0b8f5e74f9b69e92b154c8ce8
Shohruh <sharipovshohruh@gmail.com>
2025-02-20 17:22:27 +0500

    leetcode

---
+++ src/easy/math/powerofFour/Main.java
@@ -0,0 +1,9 @@
+package easy.math.powerofFour;
+
+public class Main {
+    public static void main(String[] args) {
+        int n=16;
+        Solution solution=new Solution();
+        System.out.println(solution.isPowerOfFour(n));
+    }
+}
---
+++ src/easy/math/powerofFour/Solution.java
@@ -0,0 +1,13 @@
+package easy.math.powerofFour;
+
+public class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n%4==0){
+            n=n/4;
+        }
+        return n==1;
+    }
+}
