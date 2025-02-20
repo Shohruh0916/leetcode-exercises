d34be6899b6ac6d8a0dc0b1d3b62d5500065829f
Shohruh <sharipovshohruh@gmail.com>
2025-02-20 13:08:23 +0500

    leetcode

---
+++ src/easy/math/palindromeNumber/Main.java
@@ -0,0 +1,9 @@
+package easy.math.palindromeNumber;
+
+public class Main {
+    public static void main(String[] args) {
+        int x=10;
+        Solution Solution = new Solution();
+        System.out.println(Solution.isPalindrome(x));
+    }
+}
