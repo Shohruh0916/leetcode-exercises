d313b2fe23c8ffbb4785cd771c7af444fbd79467
Shohruh <sharipovshohruh@gmail.com>
2025-02-20 21:59:45 +0500

    leetcode

---
+++ src/easy/math/validPerfectSquare/Main.java
@@ -0,0 +1,9 @@
+package easy.math.validPerfectSquare;
+
+public class Main {
+    public static void main(String[] args) {
+        int num=16;
+        Solution solution=new Solution();
+        System.out.println(solution.isPerfectSquare(num));
+    }
+}
