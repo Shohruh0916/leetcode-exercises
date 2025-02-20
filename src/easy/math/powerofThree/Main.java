47a070acfcb60ac11e12b2978b1d9ff694f00434
Shohruh <sharipovshohruh@gmail.com>
2025-02-20 17:16:14 +0500

    leetcode

---
+++ src/easy/math/powerofThree/Main.java
@@ -0,0 +1,9 @@
+package easy.math.powerofThree;
+
+public class Main {
+    public static void main(String[] args) {
+        int n=27;
+        Solution solution=new Solution();
+        System.out.println(solution.isPowerOfThree(n));
+    }
+}
