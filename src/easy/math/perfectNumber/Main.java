81b83a06dd191009f6cbb0a5da637e0aa9ac34d9
Shohruh <sharipovshohruh@gmail.com>
2025-02-20 21:45:16 +0500

    leetcode

---
+++ src/easy/math/perfectNumber/Main.java
@@ -0,0 +1,9 @@
+package easy.math.perfectNumber;
+
+public class Main {
+    public static void main(String[] args) {
+        int num=28;
+        Solution solution=new Solution();
+        System.out.println(solution.checkPerfectNumber(num));
+    }
+}
