668c854ce31923bdcf8228512bd5008262eead8a
Shohruh <sharipovshohruh@gmail.com>
2025-02-22 01:03:41 +0500

    leetcode

---
+++ src/easy/math/countPrimes/Main.java
@@ -0,0 +1,9 @@
+package easy.math.countPrimes;
+
+public class Main {
+    public static void main(String[] args) {
+        int n=2;
+        Solution solution=new Solution();
+        System.out.println(solution.countPrimes(n));
+    }
+}
