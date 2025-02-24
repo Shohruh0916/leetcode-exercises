42fb560a88af47f2fb3b98fdace96b71cdf6bfe6
Shohruh <sharipovshohruh@gmail.com>
2025-02-25 02:12:49 +0500

    leetcode

---
+++ src/easy/string/toLowerCase/Main.java
@@ -0,0 +1,9 @@
+package easy.string.toLowerCase;
+
+public class Main {
+    public static void main(String[] args) {
+        String s="Hello";
+        Solution solution=new Solution();
+        System.out.println(solution.toLowerCase(s));
+    }
+}
---
+++ src/easy/string/toLowerCase/Solution.java
@@ -0,0 +1,7 @@
+package easy.string.toLowerCase;
+
+public class Solution {
+    public String toLowerCase(String s) {
+       return s=s.toLowerCase();
+    }
+}
