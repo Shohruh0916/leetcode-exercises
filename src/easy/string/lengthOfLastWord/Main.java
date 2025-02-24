6dc401fc86aa81cd820225b0bce8bafe91fa3d93
Shohruh <sharipovshohruh@gmail.com>
2025-02-25 01:17:36 +0500

    leetcode

---
+++ src/easy/string/lengthOfLastWord/Main.java
@@ -0,0 +1,9 @@
+package easy.string.lengthOfLastWord;
+
+public class Main {
+    public static void main(String[] args) {
+        String s="Hello world";
+        Solution solution=new Solution();
+        System.out.println(solution.lengthOfLastWord(s));
+    }
+}
