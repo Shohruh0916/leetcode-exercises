671f96041dbeaabe7c65b732a3a3f6d46d9b995e
Shohruh <sharipovshohruh@gmail.com>
2025-02-25 01:51:52 +0500

    leetcode

---
+++ src/easy/string/reverseString/Main.java
@@ -0,0 +1,9 @@
+package easy.string.reverseString;
+
+public class Main {
+    public static void main(String[] args) {
+        char [] s=new char[]{'h','e','l','l','o'};
+        Solution solution=new Solution();
+        System.out.println(solution.reverseString(s));
+    }
+}
