e2a5dfa30d34930766f4dae8b2fe24a795800a05
Shohruh <sharipovshohruh@gmail.com>
2025-02-25 22:02:42 +0500

    leetcode

---
+++ src/easy/string/faultyKeyboard/Main.java
@@ -0,0 +1,9 @@
+package easy.string.faultyKeyboard;
+
+public class Main {
+    public static void main(String[] args) {
+        String s="string";
+        Solution solution=new Solution();
+        System.out.println(solution.finalString(s));
+    }
+}
---
+++ src/easy/string/faultyKeyboard/Solution.java
@@ -0,0 +1,15 @@
+package easy.string.faultyKeyboard;
+
+public class Solution {
+    public String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ch == 'i') {
+                sb.reverse();
+            } else {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
