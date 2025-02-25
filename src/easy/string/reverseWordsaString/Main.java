841dbe103ef3c1fef185c28079352fe770f0a8cf
Shohruh <sharipovshohruh@gmail.com>
2025-02-25 11:18:56 +0500

    leetcode

---
+++ src/easy/string/reverseWordsaString/Main.java
@@ -0,0 +1,9 @@
+package easy.string.reverseWordsaString;
+
+public class Main {
+    public static void main(String[] args) {
+        String s="Let's take LeetCode contest";
+        Solution solution=new Solution();
+        System.out.println(solution.reverseWords(s));
+    }
+}
