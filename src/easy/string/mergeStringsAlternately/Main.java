4097ffd724038c360261dca9a17480f8ba55ef72
Shohruh <sharipovshohruh@gmail.com>
2025-02-25 12:10:42 +0500

    leetcode

---
+++ src/easy/string/mergeStringsAlternately/Main.java
@@ -0,0 +1,10 @@
+package easy.string.mergeStringsAlternately;
+
+public class Main {
+    public static void main(String[] args) {
+        String word1="abc";
+        String word2="pqr";
+        Solution solution=new Solution();
+        System.out.println(solution.mergeAlternately(word1,word2));
+    }
+}
