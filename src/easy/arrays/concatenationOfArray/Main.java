66b39e6b64ce2d7c2e7b3f261e551dccdf615175
Shohruh <sharipovshohruh@gmail.com>
2025-02-28 16:00:35 +0500

    leetcode

---
+++ src/easy/arrays/concatenationOfArray/Main.java
@@ -0,0 +1,12 @@
+package easy.arrays.concatenationOfArray;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] nums=new int[] {1,2,1};
+        Solution solution=new Solution();
+        System.out.println(Arrays.toString(solution.getConcatenation(nums)));
+
+    }
+}
