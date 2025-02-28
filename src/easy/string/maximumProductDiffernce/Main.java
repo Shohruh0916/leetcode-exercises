7aeeb3e016d7e455070edaf2684e03ded03f6d31
Shohruh <sharipovshohruh@gmail.com>
2025-02-28 15:42:14 +0500

    leetcode

---
+++ src/easy/string/maximumProductDiffernce/Main.java
@@ -0,0 +1,9 @@
+package easy.string.maximumProductDiffernce;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] nums=new int[]{5,6,2,7,4};
+        Solution solution=new Solution();
+        System.out.println(solution.maxProductDifference(nums));
+    }
+}
