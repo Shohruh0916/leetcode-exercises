c415107af4b2d4cf8cfc0343a38ba72d404dd25e
Shohruh <sharipovshohruh@gmail.com>
2025-03-02 04:11:33 +0500

    leetcode

---
+++ src/easy/math/sumMultiples/Main.java
@@ -0,0 +1,9 @@
+package easy.math.sumMultiples;
+
+public class Main {
+    public static void main(String[] args) {
+        int n=7;
+        Solution solution=new Solution();
+        System.out.println(solution.sumOfMultiples(n));
+    }
+}
