45bf72a6a3eee30d4a3d39deb5cab7f788b57a2e
Shohruh <sharipovshohruh@gmail.com>
2025-03-02 03:53:13 +0500

    leetcode

---
+++ src/easy/math/numberOfStepstoReduce/Main.java
@@ -0,0 +1,11 @@
+package easy.math.numberOfStepstoReduce;
+
+public class Main {
+    public static void main(String[] args) {
+        int num=14;
+        Solution solution=new Solution();
+        System.out.println(solution.numberOfSteps(num));
+
+    }
+
+}
