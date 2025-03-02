669d91137014f537637314e64c5b65ca35172b51
Shohruh <sharipovshohruh@gmail.com>
2025-03-02 12:55:54 +0500

    leetcode

---
+++ src/easy/math/findTheMaximum/Main.java
@@ -0,0 +1,9 @@
+package easy.math.findTheMaximum;
+
+public class Main {
+    public static void main(String[] args) {
+        int num=4,t=1;
+        Solution solution=new Solution();
+        System.out.println(solution.theMaximumAchievableX(num,t));
+    }
+}
