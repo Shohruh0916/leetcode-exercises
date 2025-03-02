e341d55f402a37c816ffa2efb8aa3ce6df406b97
Shohruh <sharipovshohruh@gmail.com>
2025-03-02 20:18:23 +0500

    leetcode

---
+++ src/easy/math/harshadNumber/Main.java
@@ -0,0 +1,9 @@
+package easy.math.harshadNumber;
+
+public class Main {
+    public static void main(String[] args) {
+        int x=18;
+        Solution solution=new Solution();
+        System.out.println(solution.sumOfTheDigitsOfHarshadNumber(x));
+    }
+}
