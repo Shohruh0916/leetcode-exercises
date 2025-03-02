a5541d0cdbd9ab71085f421d65f25ca66e03174a
Shohruh <sharipovshohruh@gmail.com>
2025-03-02 13:06:05 +0500

    leetcode

---
+++ src/easy/math/divisbleAndNonDivisible/Main.java
@@ -0,0 +1,9 @@
+package easy.math.divisbleAndNonDivisible;
+
+public class Main {
+    public static void main(String[] args) {
+        int n=10,m=3;
+        Solution solution=new Solution();
+        System.out.println(solution.differenceOfSums(n,m));
+    }
+}
