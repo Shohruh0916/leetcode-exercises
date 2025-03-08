6d8fa2e0ad56867b93e951f2b60f6667ac80f9d3
Shohruh <sharipovshohruh@gmail.com>
2025-03-08 21:33:47 +0500

    leetcode

---
+++ src/easy/arrays/transformArrayByParity/Main.java
@@ -0,0 +1,11 @@
+package easy.arrays.transformArrayByParity;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] nums={4,3,2,1};
+        Solution solution=new Solution();
+        System.out.println(Arrays.toString(solution.transformArray(nums)));
+    }
+}
