ee05937f9b2e9dfed8279ec029054dfc61230278
Shohruh <sharipovshohruh@gmail.com>
2025-03-11 00:55:03 +0500

    leetcode

---
+++ src/easy/string/splitAStringInBalancedStrings/Main.java
@@ -0,0 +1,9 @@
+package easy.string.splitAStringInBalancedStrings;
+
+public class Main {
+    public static void main(String[] args) {
+        String s="RLRRLLRLRL";
+        Solution solution=new Solution();
+        System.out.println(solution.balancedStringSplit(s));
+    }
+}
