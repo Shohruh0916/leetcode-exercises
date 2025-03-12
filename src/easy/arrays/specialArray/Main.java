8f0492986db817d564e1c28d6d65e8a05a8e05d0
Shohruh <sharipovshohruh@gmail.com>
2025-03-12 17:29:14 +0500

    leetcode

---
+++ src/easy/arrays/specialArray/Main.java
@@ -0,0 +1,9 @@
+package easy.arrays.specialArray;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] nums={1};
+        Solution solution=new Solution();
+        System.out.println(solution.isArraySpecial(nums));
+    }
+}
---
+++ src/easy/arrays/specialArray/Solution.java
@@ -0,0 +1,12 @@
+package easy.arrays.specialArray;
+
+public class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for(int i=0; i<nums.length-1; i++){
+            if((nums[i]%2==0 && nums[i+1]%2==0) || (nums[i]%2!=0 && nums[i+1]%2!=0)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
