f52355bda420aa9ccad386400b12d875c24d19fa
Shohruh <sharipovshohruh@gmail.com>
2025-03-16 01:16:20 +0500

    leetcode

---
+++ src/easy/string/removeTrailingZerosFromAString/Main.java
@@ -0,0 +1,9 @@
+package easy.string.removeTrailingZerosFromAString;
+
+public class Main {
+    public static void main(String[] args) {
+        String num="51230100";
+        Solution solution=new Solution();
+        System.out.println(solution.removeTrailingZeros(num));
+    }
+}
