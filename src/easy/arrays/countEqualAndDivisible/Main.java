4e2c0d82ab7b4a42d18666d887a44d9a48eec19c
Shohruh <sharipovshohruh@gmail.com>
2025-03-25 13:29:17 +0500

    leetcode

---
+++ src/easy/arrays/countEqualAndDivisible/Main.java
@@ -0,0 +1,10 @@
+package easy.arrays.countEqualAndDivisible;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] nums=new int[]{5,5,9,2,5,5,9,2,2,5,5,6,2,2,5,2,5,4,3};
+        int k=7;
+        Solution solution=new Solution();
+        System.out.println(solution.countPairs(nums,k));
+    }
+}
