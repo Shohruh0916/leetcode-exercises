f157da9f1e97b3d704efe28e7cd1c8561c4420b7
Shohruh <sharipovshohruh@gmail.com>
2025-03-25 14:31:45 +0500

    leetcode

--- src/easy/arrays/shuffleString/Solution.java
+++ src/easy/arrays/shuffleString/Solution.java
@@ -2,10 +2,10 @@ package easy.arrays.shuffleString;
 
 public class Solution {
     public String restoreString(String s, int[] indices) {
-        char[] shuffled=new char[s.length()];
+        char[] a=new char[s.length()];
         for (int i=0; i<s.length(); i++) {
-            shuffled[indices[i]]=s.charAt(i);
+            a[indices[i]]=s.charAt(i);
         }
-        return new String(shuffled);
+        return new String(a);
     }
 }
