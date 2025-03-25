438e426dc07e7963f8649a4259c7e80da218497e
Shohruh <sharipovshohruh@gmail.com>
2025-03-25 21:09:46 +0500

    leetcode

---
+++ src/medium/math/checkIfNumberIsASumOfPowersOfThree/Main.java
@@ -0,0 +1,9 @@
+package medium.math.checkIfNumberIsASumOfPowersOfThree;
+
+public class Main {
+    public static void main(String[] args) {
+        int n=21;
+        Solution solution=new Solution();
+        System.out.println(solution.checkPowersOfThree(n));
+    }
+}
