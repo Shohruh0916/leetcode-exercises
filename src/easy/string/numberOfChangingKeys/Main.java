0245db4ccb731715c30640d59226f49e46a55f25
Shohruh <sharipovshohruh@gmail.com>
2025-03-28 12:29:44 +0500

    leetcode

---
+++ src/easy/string/numberOfChangingKeys/Main.java
@@ -0,0 +1,9 @@
+package easy.string.numberOfChangingKeys;
+
+public class Main {
+    public static void main(String[] args) {
+        String s="aAbBcC";
+        Solution solution=new Solution();
+        System.out.println(solution.countKeyChanges(s));
+    }
+}
---
+++ src/easy/string/numberOfChangingKeys/Solution.java
@@ -0,0 +1,13 @@
+package easy.string.numberOfChangingKeys;
+
+public class Solution {
+    public int countKeyChanges(String s) {
+        int count=0;
+        for(int i=0; i<s.length()-1; i++){
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(i+1))){
+                count++;
+            }
+        }
+        return count;
+    }
+}
