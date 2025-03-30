c35fa23db0823f7008eafc5cf9cc1611d9499113
Shohruh <sharipovshohruh@gmail.com>
2025-03-30 18:02:19 +0500

    leetcode

---
+++ src/easy/string/fizzBuzz/Main.java
@@ -0,0 +1,9 @@
+package easy.string.fizzBuzz;
+
+public class Main {
+    public static void main(String[] args) {
+        int n=3;
+        Solution solution=new Solution();
+        System.out.println(solution.fizzBuzz(n));
+    }
+}
