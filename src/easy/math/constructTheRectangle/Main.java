bfd788460540b05aac52c96a96a7af02d4f63d1d
Shohruh <sharipovshohruh@gmail.com>
2025-04-07 11:55:41 +0500

    leetcode

---
+++ src/easy/math/constructTheRectangle/Main.java
@@ -0,0 +1,11 @@
+package easy.math.constructTheRectangle;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int area=4;
+        Solution solution=new Solution();
+        System.out.println(Arrays.toString(solution.constructRectangle(area)));
+    }
+}
---
+++ src/easy/math/constructTheRectangle/Solution.java
@@ -0,0 +1,15 @@
+package easy.math.constructTheRectangle;
+
+public class Solution {
+    public int[] constructRectangle(int area) {
+        int [] result=new int[2];
+        int s1=(int)Math.sqrt(area);
+        while(area%s1!=0){
+            s1--;
+        }
+        int s2=area/s1;
+        result[0]=s2;
+        result[1]=s1;
+        return result;
+    }
+}
