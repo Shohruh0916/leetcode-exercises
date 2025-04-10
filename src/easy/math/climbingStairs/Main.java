dfd1a5edbe8f9c8a3b286618f6bd3635edcf7f6f
Shohruh <sharipovshohruh@gmail.com>
2025-04-10 16:00:58 +0500

    leetcode

---
+++ src/easy/math/climbingStairs/Main.java
@@ -0,0 +1,9 @@
+package easy.math.climbingStairs;
+
+public class Main {
+    public static void main(String[] args) {
+        int n=2;
+        Solution solution=new Solution();
+        System.out.println(solution.climbStairs(n));
+    }
+}
