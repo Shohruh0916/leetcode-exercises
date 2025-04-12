59ee61f55ef1e508e7a1e11eabeb0a8b46737557
Shohruh <sharipovshohruh@gmail.com>
2025-04-12 14:30:08 +0500

    leetcode

---
+++ src/easy/math/findNumbersWithEvenNumberOfDigits/Main.java
@@ -0,0 +1,9 @@
+package easy.math.findNumbersWithEvenNumberOfDigits;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] nums= new int[]{555,901,482,1771};
+        Solution solution=new Solution();
+        System.out.println(solution.findNumbers(nums));
+    }
+}
---
+++ src/easy/math/findNumbersWithEvenNumberOfDigits/Solution.java
@@ -0,0 +1,14 @@
+package easy.math.findNumbersWithEvenNumberOfDigits;
+
+public class Solution {
+    public int findNumbers(int[] nums) {
+        int count=0;
+        for(int num : nums) {
+            int digitCount=String.valueOf(num).length();
+            if (digitCount%2==0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
