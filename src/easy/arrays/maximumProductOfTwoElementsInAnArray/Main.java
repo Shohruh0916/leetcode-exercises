e95f5e6ee5e656806283e34e7bdc0542b22bf6b1
Shohruh <sharipovshohruh@gmail.com>
2025-04-15 10:59:32 +0500

    leetcode

---
+++ src/easy/arrays/maximumProductOfTwoElementsInAnArray/Main.java
@@ -0,0 +1,9 @@
+package easy.arrays.maximumProductOfTwoElementsInAnArray;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] nums=new int[]{3,4,5,2};
+        Solution solution=new Solution();
+        System.out.println(solution.maxProduct(nums));
+    }
+}
